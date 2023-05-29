264fb1187413db040e51cb3c470671192661840e
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-05-29 18:01:30 +0600

    [DONE] setup eslint, pritter, lint-stage, husky

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
-const port = 5000;
+const app: Application = express()
 
 // Coross Origine
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
