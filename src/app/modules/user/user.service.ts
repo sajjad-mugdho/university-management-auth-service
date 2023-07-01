58ecc2e25ac8e92ee39dc95644b3d7ed44c9a66d
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-07-02 03:58:37 +0600

    [Done] Create user with transection and roll back

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,27 +1,90 @@
+import mongoose from 'mongoose';
 import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { IStudent } from '../student/student.interface';
 import { IUser } from './user.interface';
 import { User } from './user.model';
-import { genareteUserID } from './user.utils';
+import { genareteStudentID } from './user.utils';
+import { Student } from '../student/student.model';
+import httpStatus from 'http-status';
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
 
-const createUserDB = async (user: IUser): Promise<IUser | null> => {
-  //Auto genareted incrimental ID
-  const id = await genareteUserID();
-
-  user.id = id;
-  //Auto password
+const createStudent = async (
+  student: IStudent,
+  user: IUser
+): Promise<IUser | null> => {
+  // default password
   if (!user.password) {
-    user.password = config.default_user_password as string;
+    user.password = config.default_student_password as string;
   }
-  const createdUser = await User.create(user);
+  // set role
+  user.role = 'student';
 
-  if (!createUserDB) {
-    throw new ApiError(400, 'Faild to create user', '');
+  const academicsemester = await AcademicSemester.findById(
+    student.academicSemester
+  ).lean();
+
+  // generate student id
+  let newUserAllData = null;
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+
+    const id = await genareteStudentID(academicsemester as IAcademicSemester);
+
+    user.id = id;
+    student.id = id;
+
+    //array
+    const newStudent = await Student.create([student], { session });
+
+    if (!newStudent.length) {
+      throw new ApiError(
+        httpStatus.BAD_REQUEST,
+        'Failed to create student',
+        ''
+      );
+    }
+
+    //set student -->  _id into user.student
+    user.student = newStudent[0]._id;
+
+    const newUser = await User.create([user], { session });
+
+    if (!newUser.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create user', '');
+    }
+    newUserAllData = newUser[0];
+
+    await session.commitTransaction();
+    await session.endSession();
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
 
-  return createdUser;
+  if (newUserAllData) {
+    newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
+      path: 'student',
+      populate: [
+        {
+          path: 'academicSemester',
+        },
+        {
+          path: 'academicDepartment',
+        },
+        {
+          path: 'academicFaculty',
+        },
+      ],
+    });
+  }
+
+  return newUserAllData;
 };
 
 export const UserService = {
-  createUserDB,
+  createStudent,
 };
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,15 +1,51 @@
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { User } from './user.model';
 
-export const findLastUserId = async () => {
-  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+export const findLastStudentId = async (): Promise<string | undefined> => {
+  const lastStudent = await User.findOne(
+    {
+      role: 'student',
+    },
+    { id: 1, _id: 0 }
+  )
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastUser?.id;
+  return lastStudent?.id ? lastStudent.id.substring(4) : undefined;
 };
 
-export const genareteUserID = async () => {
-  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0');
-  const incrimentedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+export const genareteStudentID = async (
+  academicSemester: IAcademicSemester | null
+) => {
+  const currentId =
+    (await findLastStudentId()) || (0).toString().padStart(5, '0');
+  let incrimentedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+
+  incrimentedId = `${academicSemester?.year.substring(2)}${
+    academicSemester?.code
+  }${incrimentedId}`;
+
+  return incrimentedId;
+};
+
+export const findLastFacultyId = async (): Promise<string | undefined> => {
+  const lastFaculty = await User.findOne(
+    {
+      role: 'faculty',
+    },
+    { id: 1, _id: 0 }
+  )
+    .sort({ createdAt: -1 })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const genareteFacultyId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0');
+  let incrimentedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+
+  incrimentedId = `F-${incrimentedId}`;
   return incrimentedId;
 };
